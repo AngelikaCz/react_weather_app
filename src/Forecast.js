6a6ea317d6324f4fc8affa07224d90334afba025
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-10-28 20:31:55 +0200

    Set states and added forecast days

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,12 +5,21 @@ import "./Forecast.css";
 
 export default function Forecast(props) {
   let [temperature, setTemperature] = useState(null);
+  let [icon, setIcon] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
   let url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=dee9a420d2a7b5a314d3260f8ca83eea&units=metric`;
-  function showTemperature(response) {
-    setTemperature(response.data.main.temp);
+  function showWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setIcon(response.data.weather[0].icon);
+    setDescription(response.data.weather[0].description.toUpperCase());
+    setHumidity();
+    setWind();
   }
-  temperature = "Paris";
-  axios.get(url).then(showTemperature);
+
+  let city = "Paris";
+  axios.get(url).then(showWeather);
   return (
     <div>
       <form className="Header">
@@ -34,19 +43,26 @@ export default function Forecast(props) {
         </div>
       </form>{" "}
       <div className="city">
-        <span id="city">{temperature}</span>,{" "}
-        <span id="date">Sunday 14:00</span>
+        <span id="city">{city}</span>, <span id="date">Sunday 14:00</span>
       </div>
-      <div id="description">Cloudy</div>
+      <div id="description">{description}</div>
       <div className="temperature">
-        <span id="temperature">☀19</span>°<span className="units">C</span>
+        <span id="temperature">☀{temperature}</span>°
+        <span className="units">C</span>
       </div>
       <div className="last_section">
         Humidity <span id="humidity">60</span>% Wind
         <span id="wind"> 4</span>km/h
         <hr />
       </div>
-      <div className="forecast" id="forecast"></div>
+      <div className="forecastContainer">
+        <div className="days">Friday</div>
+        <div className="days">Saturday</div>
+        <div className="days">Sunday</div>
+        <div className="days">Monday</div>
+        <div className="days">Tuesday</div>
+        <div className="days">Wednesday</div>
+      </div>
     </div>
   );
 }
